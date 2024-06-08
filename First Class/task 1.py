f86f3198069e80cf3ac772e169eee8fde90d0825
Abul Hossain Chowdhury <fahim150283@gmail.com>
2024-06-08 17:40:07 +0600

    1st class

--- First Class/task 1.py
+++ First Class/task 1.py
@@ -1,10 +1,22 @@
+# task 1
+
 a = int(input("Please enter a number : "))
 
-if a%3 == 0:
-    print("mod 3")7
+if a%3 == 0 and a%5 == 0:
+    print("mod 3 and 5")
 
-elif a%5 == 5:
-    print("mod 5")
+elif a%3 == 0 or a%5 == 0:
+    print("mod 3 or 5")
+
+    if a%3 == 0 :
+        print("mod 3")
+
+    elif a%5 == 0:
+        print("mod 5")
 
 else:
     print("aida print hobe na")
+
+
+
+# task 2
