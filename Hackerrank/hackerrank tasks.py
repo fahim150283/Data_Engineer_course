319fe9a6e1f8c733e8cf7e3d276e87386abb1900
Abul Hossain Chowdhury <fahim150283@gmail.com>
2024-06-24 21:50:31 +0600

    introduced a folder for hackerrank solutions

---
+++ Hackerrank/hackerrank tasks.py
@@ -0,0 +1,28 @@
+###############           Task          ###########################
+#
+#           Sample Input
+#
+#           STDIN           Function
+#           -----           --------
+#           abracadabra     s = 'abracadabra'
+#           5 k             position = 5, character = 'k'
+#           Sample Output
+#
+#           abrackdabra
+
+
+###########         answer          ################
+
+def mutate_string(string, position, character):
+
+    array = list(string)
+    array[position]=character
+    String = "".join(array)
+
+    return String
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
